3d3b46eb6bd23daa6d3c7717e641335e9895141a
Viswa V <viswamalar2004@gmail.com>
2025-09-30 14:55:35 +0530

    Stable build

--- voiceproject/voiceproject/settings.py
+++ voiceproject/voiceproject/settings.py
@@ -13,7 +13,6 @@ load_dotenv(ENV_PATH)
 
 # Get API key
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
-ELEVENLABS_API_KEY = os.environ.get('ELEVENLABS_API_KEY')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
