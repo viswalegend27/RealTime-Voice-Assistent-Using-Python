7b4ecf983d5eb21b129049145f3ce9b1d0f79c54
Viswa V <viswamalar2004@gmail.com>
2025-10-08 22:36:16 +0530

    Working batch file

--- run_localhost.bat
+++ run_localhost.bat
@@ -1,15 +1,10 @@
 @echo off
-REM Change to the project folder
 cd /d "%~dp0"
-
-REM Activate the virtual environment
 call "ai-speech\Scripts\activate.bat"
-
-REM Move into project directory
 cd voiceproject
 
-REM Start the Django server
-start "" cmd /k python manage.py runserver
+REM Run with Daphne (ASGI) to support WebSockets
+start "" cmd /k daphne -p 8000 voiceproject.asgi:application
 
-REM Open the default web browser to localhost
+REM Open browser
 start http://127.0.0.1:8000/
