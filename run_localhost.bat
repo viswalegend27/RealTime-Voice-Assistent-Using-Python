9f6dcb36051d64e68609e2b89722d63d3b3eca6e
Viswa V <viswamalar2004@gmail.com>
2025-10-10 07:51:11 +0530

    modfied batch

--- run_localhost.bat
+++ run_localhost.bat
@@ -3,8 +3,8 @@ cd /d "%~dp0"
 call "ai-speech\Scripts\activate.bat"
 cd voiceproject
 
-REM Run with Daphne (ASGI) to support WebSockets
-start "" cmd /k daphne -p 8000 voiceproject.asgi:application
+REM Run with Daphne (ASGI) to support WebSockets in the current window
+daphne -p 8000 voiceproject.asgi:application
 
 REM Open browser
 start http://127.0.0.1:8000/
